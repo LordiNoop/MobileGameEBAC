4b1c69bcfdfd99178ec8ed829980c0038b59a01b
César <cesar.romero.borges@gmail.com>
2025-05-28 15:14:40 -0300

    setting animations and adding AnimatorManager

---
+++ Assets/Scripts/AnimationExample.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationExample : MonoBehaviour
+{
+    public Animation anim;
+
+    public AnimationClip run;
+    public AnimationClip idle;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayAnimation(run);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayAnimation(idle);
+        }
+    }
+
+    private void PlayAnimation(AnimationClip c)
+    {
+        anim.CrossFade(c.name);
+    }
+}
