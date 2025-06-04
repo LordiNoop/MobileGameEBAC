18db25c98ff85d33e621aabedfb2cfb8cf1c0261
César <cesar.romero.borges@gmail.com>
2025-06-04 17:46:28 -0300

    saved Scene

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -98,6 +98,7 @@ public class PlayerController : Singleton<PlayerController>
             {
                 EndGame(AnimatorManager.AnimationType.DEAD);
                 MoveBack(transform);
+                if (vfxDeath != null) vfxDeath.Play();
             }
         }
     }
@@ -112,7 +113,6 @@ public class PlayerController : Singleton<PlayerController>
         _canRun = false;
         endScreen.SetActive(true);
         animatorManager.Play(animationType);
-        if (vfxDeath != null) vfxDeath.Play();
     }
 
     public void StartToRun()
